2d2520c86dc6ccfd9a93ef2d0b95edf4e398edab
krip2nite <kochnevandrey94@gmail.com>
2025-05-11 20:43:37 +0300

    responsive page structure

--- games-hub/src/App.tsx
+++ games-hub/src/App.tsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
 import './App.css'
-import { Tooltip } from './components/ui/tooltip'
-import { Box } from '@chakra-ui/react'
+import { Grid, GridItem, Stack} from '@chakra-ui/react'
 import { ColorModeButton } from './components/ui/color-mode'
 
 function App() {
   
 
   return (
-    <>
-    <Tooltip content={"Kuku"} contentProps={{background:"red"}}showArrow>
-      <Box textAlign={"center"}>XXXXXXX</Box>
-    </Tooltip>
-    <ColorModeButton></ColorModeButton>
-    </>
+    <Grid templateAreas={{
+      base: '"nav" "main"' ,
+      md: '"nav nav" "aside main"'
+    }}>
+      <GridItem area="nav" bg="coral" >NAV
+        <ColorModeButton></ColorModeButton>
+      </GridItem>
+      <Stack hideBelow="md">
+        <GridItem area="aside" bg="gold">ASIDE</GridItem>
+      </Stack>
+      <GridItem area="main" bg="dodgerblue">MAIN</GridItem>
+    </Grid>
   )
 }
 
